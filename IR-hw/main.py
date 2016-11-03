603dd9c9749e5b754981ed890e054947ccb1476a
milletpu <milletpu@gmail.com>
2016-11-03 23:49:17 +0800

    1, add corpus; 2, test shakespeare corpus in main.

--- IR-hw/main.py
+++ IR-hw/main.py
@@ -3,22 +3,13 @@
 import textparser
 import index
 
-a = list(textparser.word_tokenize("pig's pig, hello 2 cruel's \"apples\" sisters' pig"))
+#a = list(textparser.word_tokenize("pig's pig, hello 2 cruel's \"apples\" sisters' pig"))
+
+corpus = open('shakespeare-merchant.trec.1').read().lower()
+b = list(textparser.word_tokenize(corpus))
 index = index.HashedIndex()
 
-for term in a:
+for term in b:
     index.add_term_occurrence(term[0].encode('utf-8'), 'doc1')
-index.add_term_occurrence('pig', 'doc2')
 
-index.add_term_occurrence('pig', 'doc3')
-
-print 'index.items():'
-print index.items(), '\n'
-
-print 'index.to_dict():'
-print index.to_dict(), '\n'
-
-print 'index.inverted_index():'
-print index.inverted_index(), '\n'
-
-print index.get_posting_list('pig')
+print index.inverted_index()
