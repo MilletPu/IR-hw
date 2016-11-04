19b779ca00f1192253b2548c54029cb406a7ca61
milletpu <milletpu@gmail.com>
2016-11-04 20:26:35 +0800

    main.py: successfully test on the shakespeare-merchant.trec.1

--- IR-hw/main.py
+++ IR-hw/main.py
@@ -5,7 +5,6 @@ import index
 from bs4 import BeautifulSoup
 
 
-#
 index = index.HashedIndex()
 
 file_object = open('shakespeare-merchant.trec.1')
@@ -13,7 +12,6 @@ corpus = file_object.read().lower()
 soup = BeautifulSoup(corpus, "html.parser")
 
 all_docs = soup.findAll("doc")
-
 for doc in all_docs:
     doc_content = str(doc)
     sp = BeautifulSoup(str(doc), "html.parser")
@@ -25,26 +23,19 @@ for doc in all_docs:
             index.add_term_occurrence(term[0].encode('utf-8'), doc_no_content)
 
 print index.sorted_inverted_index()
-print index.get_sorted_posting_list('a')
+print index.get_sorted_posting_list('saying')
 
 
 
 
-# index = index.HashedIndex()
-# a = list(textparser.word_tokenize("pig's pig, hello 2 cruel's \"apples\" sisters' pig"))
-# file_object = open('shakespeare-merchant.trec.1')
-#
-# try:
-#     corpus = file_object.read().lower()
-# finally:
-#      file_object.close( )
-#
-# corpus_to_token =list(textparser.word_tokenize(corpus))
-# for term in corpus_to_token:
-#     index.add_term_occurrence(term[0].encode('utf-8'), 'doc1')
-# index.add_term_occurrence('a'.encode('utf-8'), 'doc3')
-# index.add_term_occurrence('a'.encode('utf-8'), 'doc5')
-#
-#
-# print index.sorted_inverted_index()
-# print index.get_sorted_posting_list('a')
+
+
+
+
+
+
+
+
+
+
+
