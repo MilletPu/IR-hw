bf7f3664e92348bac206f2042e928d6f022b135d
milletpu <milletpu@gmail.com>
2016-11-05 02:53:04 +0800

    main.py: doc_content = str(doc) or =str(doc.contents)

--- IR-hw/main.py
+++ IR-hw/main.py
@@ -5,16 +5,15 @@ import index
 from bs4 import BeautifulSoup
 
 
-index = index.HashedIndex()
-
 file_object = open('shakespeare-merchant.trec.1')
 corpus = file_object.read().lower()
-soup = BeautifulSoup(corpus, "html.parser")
 
+index = index.HashedIndex()
+soup = BeautifulSoup(corpus, "html.parser")
 all_docs = soup.findAll("doc")
 for doc in all_docs:
-    doc_content = str(doc)
-    sp = BeautifulSoup(str(doc), "html.parser")
+    doc_content = str(doc) #or: doc_content = str(doc.contents), which means <doc> is not included
+    sp = BeautifulSoup(doc_content, "html.parser")
     all_doc_nos = sp.findAll("docno")
     doc_content_to_token = list(textparser.word_tokenize(doc_content))
     for doc_no in all_doc_nos:
@@ -23,8 +22,7 @@ for doc in all_docs:
             index.add_term_occurrence(term[0].encode('utf-8'), doc_no_content)
 
 print index.sorted_inverted_index()
-print index.get_sorted_posting_list('saying')
-
+print index.get_sorted_posting_list('title')
 
 
 
