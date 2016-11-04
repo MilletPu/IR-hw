9fb7f170cf9453e30e9786df240956596bca8ed0
milletpu <milletpu@gmail.com>
2016-11-04 17:20:38 +0800

    main.py: successfully test on the shakespeare-merchant.trec.1

--- IR-hw/main.py
+++ IR-hw/main.py
@@ -2,27 +2,49 @@
 
 import textparser
 import index
-import collections
+from bs4 import BeautifulSoup
 
-a = list(textparser.word_tokenize("pig's pig, hello 2 cruel's \"apples\" sisters' pig"))
 
+#
+index = index.HashedIndex()
+
+file_object = open('shakespeare-merchant.trec.1')
+corpus = file_object.read().lower()
+soup = BeautifulSoup(corpus, "html.parser")
+
+all_docs = soup.findAll("doc")
+
+for doc in all_docs:
+    doc_content = str(doc)
+    sp = BeautifulSoup(str(doc), "html.parser")
+    all_doc_nos = sp.findAll("docno")
+    doc_content_to_token = list(textparser.word_tokenize(doc_content))
+    for doc_no in all_doc_nos:
+        doc_no_content = doc_no.contents[0].encode('utf-8')
+        for term in doc_content_to_token:
+            index.add_term_occurrence(term[0].encode('utf-8'), doc_no_content)
+
+print index.sorted_inverted_index()
+print index.get_sorted_posting_list('a')
+
+
+
+
+# index = index.HashedIndex()
+# a = list(textparser.word_tokenize("pig's pig, hello 2 cruel's \"apples\" sisters' pig"))
 # file_object = open('shakespeare-merchant.trec.1')
+#
 # try:
 #     corpus = file_object.read().lower()
 # finally:
 #      file_object.close( )
 #
-# corpus_to_token = list(textparser.word_tokenize(corpus))
-
-index = index.HashedIndex()
-
-for term in a:
-    index.add_term_occurrence(term[0].encode('utf-8'), 'doc1')
-index.add_term_occurrence('pig'.encode('utf-8'), 'doc3')
-index.add_term_occurrence('pig'.encode('utf-8'), 'doc5')
-
-print index.inverted_index()
-print index.sorted_inverted_index
-print index.sorted_inverted_index['apples']
-print index.get_sorted_posting_list('apples')
-print index.get_sorted_posting_list('pig')
+# corpus_to_token =list(textparser.word_tokenize(corpus))
+# for term in corpus_to_token:
+#     index.add_term_occurrence(term[0].encode('utf-8'), 'doc1')
+# index.add_term_occurrence('a'.encode('utf-8'), 'doc3')
+# index.add_term_occurrence('a'.encode('utf-8'), 'doc5')
+#
+#
+# print index.sorted_inverted_index()
+# print index.get_sorted_posting_list('a')
